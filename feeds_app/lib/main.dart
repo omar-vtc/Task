b2fd76b8ea9df8b5a2d181e0d02ed531b5a581b2
Omar Khaled <72626558+omar-vtc@users.noreply.github.com>
2025-06-02 21:28:58 +0300

    base

--- feeds_app/lib/main.dart
+++ feeds_app/lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(const MyApp());
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("hello Sarah")));
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Hello Sarah")),
+        body: const Placeholder(),
+      ),
+    );
   }
 }
